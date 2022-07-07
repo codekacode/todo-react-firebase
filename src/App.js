241a95299a4e3da52886fb0ef966b06a63afe5ad
Codekacode <karem.quiroz.m@gmail.com>
2022-07-06 23:53:20 -0500

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@mui/material';
+import { useState } from 'react';
+import Todo from './Todo';
 
 function App() {
+
+  const [todos, setTodos] = useState(['task1', 'task2']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      input
+    ])
+    setInput('');   
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={e => setInput(e.target.value)} />
+        </FormControl>
+      </form>
+      <Button disabled={!input} type="submit" className="button__addtodo" onClick={addTodo} variant="contained">Add Todo</Button>
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo}/>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,8 @@
+
+function Todo({text}) {
+  return (
+    <div>{text}</div>
+  )
+}
+
+export default Todo
